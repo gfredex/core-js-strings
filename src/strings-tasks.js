e4586f28772c7069b34b34c478ce784153a53550
Dima Zemnitski <g.fredex@gmail.com>
2023-12-16 23:16:04 +0300

    feat: add solution function sumOfCodes(str)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -199,8 +199,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let result = 0;
+  if (str === undefined || str === null) {
+    return 0;
+  }
+  for (let i = 0; i < str.length; i += 1) {
+    result += str.charCodeAt(i);
+  }
+  return result;
 }
 
 /**
